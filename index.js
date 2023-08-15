4ddde0aca1780f45bad1fbe8d2dd697134322d16
Ryan <rom@ualberta.ca>
2023-08-15 15:30:10 -0600

    t

--- index.js
+++ index.js
@@ -22,39 +22,48 @@ var canvasImageSchema = new mongoose.Schema({
 
 var CanvasImage = mongoose.model('canvasimages', canvasImageSchema);
 
-var canvas = createCanvas(750, 750);
-var ctx = canvas.getContext("2d");
-var base64ImageData = "";
-
 
 io.on('connection', async(socket)=>{
         console.log('a user connected');
         socket.on("canvas-data", ()=>{
-            socket.emit("canvas-data", base64ImageData );
-            console.log("canvas-data-emitted");
+            CanvasImage.find({_id:"frequentSave"}).then((docs)=>{
+                var base64ImageData = docs[0].image;
+                socket.emit("canvas-data", base64ImageData );
+                console.log("canvas-data-emitted");
+            })
         })
 
         socket.on("pen-action", (line_details_array) => {
             console.log("pen-action")
-            for (let i = 0; i < line_details_array.length; i++) {
-                var line_details = line_details_array[i];
-                ctx.lineWidth = line_details.lineWidth;
-                ctx.lineJoin = line_details.lineJoin;
-                ctx.lineCap = line_details.lineCap;
-                ctx.strokeStyle = line_details.strokeStyle;
-                ctx.beginPath();
-                ctx.moveTo(line_details.startX, line_details.startY);
-                ctx.lineTo(line_details.currX, line_details.currY);
-                ctx.closePath();
-                ctx.stroke();
-            }
-            console.log("drawn to canvas")
-            base64ImageData = canvas?.toDataURL("image/png");
+            CanvasImage.find({_id:"frequentSave"}).then((docs)=>{
+                var base64ImageData = docs[0].image;
+                var canvas = createCanvas(750, 750);
+                var ctx = canvas.getContext("2d");
+                var image = new Image();
+                image.onload = function(){
+                    ctx?.drawImage(image, 0, 0);
+                };
+                image.src = data;
+                 for (let i = 0; i < line_details_array.length; i++) {
+                    var line_details = line_details_array[i];
+                    ctx.lineWidth = line_details.lineWidth;
+                    ctx.lineJoin = line_details.lineJoin;
+                    ctx.lineCap = line_details.lineCap;
+                    ctx.strokeStyle = line_details.strokeStyle;
+                    ctx.beginPath();
+                    ctx.moveTo(line_details.startX, line_details.startY);
+                    ctx.lineTo(line_details.currX, line_details.currY);
+                    ctx.closePath();
+                    ctx.stroke();
+                }
+                console.log("drawn to canvas")
+                base64ImageData = canvas?.toDataURL("image/png");
 
-            var filter = { _id: "frequentSave" };
-            var update = { image: base64ImageData };
-            CanvasImage.findOneAndUpdate(filter, update).then(()=>{
-                console.log("updated database")
+                var filter = { _id: "frequentSave" };
+                var update = { image: base64ImageData };
+                CanvasImage.findOneAndUpdate(filter, update).then(()=>{
+                    console.log("updated database")
+                })
             })
         })
     })
@@ -62,8 +71,6 @@ io.on('connection', async(socket)=>{
 const start = async() => {
     try {
         await mongoose.connect(process.env.MONGOOSE_URI);
-        var docs = await CanvasImage.find({_id:"frequentSave"});
-        base64ImageData = docs[0].image;
 
         httpServer.listen(5000, ()=> console.log('started on 5000'))
     }
