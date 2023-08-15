8b91067da50c580f70e20d4450c45b488d156d00
Ryan <rom@ualberta.ca>
2023-08-15 04:11:46 -0600

    Changed drawing implementation

--- index.js
+++ index.js
@@ -21,51 +21,34 @@ var canvasImageSchema = new mongoose.Schema({
 
 var CanvasImage = mongoose.model('canvasimages', canvasImageSchema);
 
+var canvas = createCanvas(750, 750);
+var ctx = canvas.getContext("2d");
+
 io.on('connection', (socket)=>{
     console.log('a user connected');
 
     socket.on("canvas-data", ()=>{
-        CanvasImage.find({_id:"frequentSave"}).then((docs)=>{
-            console.log("got from database")
-            var base64ImageData = docs[0].image;
-            socket.emit("canvas-data", base64ImageData );
-        })
+        var base64ImageData = canvas?.toDataURL("image/png");
+        socket.emit("canvas-data", base64ImageData );
     })
 
-    socket.on("pen-action", (draw_options_array) => {
+    socket.on("pen-action", (line_details) => {
         console.log("pen-action")
-        CanvasImage.find({_id:"frequentSave"}).then((docs)=>{
-            console.log("got from database")
-            var base64ImageData = docs[0].image;
-            loadImage(base64ImageData).then((image)=>{
-                const canvas = createCanvas(750, 750)
-                var ctx = canvas.getContext('2d');
-                ctx.drawImage(image, 0, 0)
-                for(var i = 0;i<draw_options_array.length;i++) {
-                    var draw_options = draw_options_array[i];
-                    ctx.lineWidth = draw_options.lineWidth;
-                    ctx.lineJoin = draw_options.lineJoin;
-                    ctx.lineCap = draw_options.lineCap;
-                    ctx.strokeStyle = draw_options.strokeStyle;
 
-                    ctx.beginPath();
-                    ctx.moveTo(draw_options.startX, draw_options.startY);
-                    ctx.lineTo(draw_options.currX, draw_options.currY);
-                    ctx.closePath();
-                    ctx.stroke();
-                }
-                var base64ImageData = canvas?.toDataURL("image/png");
+        //draw to canvas
+        ctx.lineWidth = draw_options.lineWidth;
+        ctx.lineJoin = draw_options.lineJoin;
+        ctx.lineCap = draw_options.lineCap;
+        ctx.strokeStyle = draw_options.strokeStyle;
+        ctx.beginPath();
+        ctx.moveTo(line_details.startX, line_details.startY);
+        ctx.lineTo(line_details.currX, line_details.currY);
+        ctx.closePath();
+        ctx.stroke();
 
-                socket.broadcast.emit("canvas-data", base64ImageData );
-
-                var filter = { _id: "frequentSave" };
-                var update = { image: base64ImageData };
-                CanvasImage.findOneAndUpdate(filter, update).then(()=>{
-                    console.log("updated database")
-                })
-            })
-        })
-        
+        //notify views
+        var base64ImageData = canvas?.toDataURL("image/png");
+        io.emit("canvas-data", base64ImageData );
     })
 })
 
